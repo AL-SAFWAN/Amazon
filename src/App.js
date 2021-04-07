e8d194a9bb26c693efc7dc986464dd878e05d11f
AL-SAFWAN <al-safwan.uddin@abehr.com>
2021-04-07 19:21:35 +0100

    homePage with the product component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     //BEM
-    <div className="app">
-      <Header></Header>
-      {/* Header */}
-      {/* Home */}
-      {/*  */}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <div className="app">
+            <Header />
+            <Home />
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
